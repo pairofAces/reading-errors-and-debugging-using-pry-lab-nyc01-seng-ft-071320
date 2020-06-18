5f42512640e1c5698bbbd0251e27319a261dc4e3
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-18 15:54:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,11 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  num = 1
   if num = 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    puts ""
+    puts "HAM HAM HAM IN MY TUMMY"
 
   end
 end
